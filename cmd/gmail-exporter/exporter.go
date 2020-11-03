a72b53c26370eb5103413f58d670e6acb881d4c1
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2020-11-03 19:07:50 +0000

    Improved logging

--- cmd/gmail-exporter/exporter.go
+++ cmd/gmail-exporter/exporter.go
@@ -34,7 +34,7 @@ var refreshLabelsErrs = prometheus.NewCounter(prometheus.CounterOpts{
 })
 
 // Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) (*http.Client, error) {
+func getClient(logger log.Logger, config *oauth2.Config) (*http.Client, error) {
 	// The file token.json stores the user's access and refresh tokens, and is
 	// created automatically when the authorization flow completes for the first
 	// time.
@@ -42,7 +42,7 @@ func getClient(config *oauth2.Config) (*http.Client, error) {
 	tok, err := tokenFromFile(tokFile)
 	if err != nil {
 		tok, _ = getTokenFromWeb(config)
-		if err := saveToken(tokFile, tok); err != nil {
+		if err := saveToken(logger, tokFile, tok); err != nil {
 			return nil, errors.Wrap(err, "saving token")
 		}
 	}
@@ -80,15 +80,14 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 }
 
 // Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) error {
-	fmt.Printf("Saving credential file to: %s\n", path)
+func saveToken(logger log.Logger, path string, token *oauth2.Token) error {
+	_ = logger.Log(log.Message("Saving credential file"))
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		return fmt.Errorf("Unable to cache oauth token: %v", err)
+		return fmt.Errorf("unable to cache oauth token: %v", err)
 	}
 	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-	return nil
+	return errors.Wrap(json.NewEncoder(f).Encode(token), "encoding token to JSON")
 }
 
 func run(logger log.Logger) func(*cobra.Command, []string) error {
@@ -109,7 +108,7 @@ func run(logger log.Logger) func(*cobra.Command, []string) error {
 			return fmt.Errorf("unable to parse client secret file to config: %v", err)
 		}
 
-		client, err := getClient(config)
+		client, err := getClient(logger, config)
 		if err != nil {
 			return errors.Wrap(err, "getting client")
 		}
@@ -147,7 +146,12 @@ func run(logger log.Logger) func(*cobra.Command, []string) error {
 		)))
 
 		addr := ":8765"
-		fmt.Println("listening at ", addr)
+		if err := logger.Log(log.Message("listening"), log.KV{
+			K: "address",
+			V: addr,
+		}); err != nil {
+			return errors.Wrap(err, "logging")
+		}
 		return errors.Wrap(http.ListenAndServe(addr, nil), "serving")
 	}
 }
@@ -189,10 +193,10 @@ func (e *exporter) Collect(metrics chan<- prometheus.Metric) {
 	metricsCount := len(e.refreshingLabels) * metricsPerLabel
 	ms := make(chan prometheus.Metric, metricsCount)
 	for _, label := range e.refreshingLabels {
-		lData, err := e.Get(userID, label.id).Do()
+		lData, err := e.Get(userID, label.Id).Do()
 		if err != nil {
 			// how do I return an error so that it will be shown in prometheus here?
-			e.logger.Log(log.Error(fmt.Errorf("getting label data for label: %s: %v", label.id, err)))
+			e.logger.Log(log.Error(fmt.Errorf("getting label data for label: %s: %v", label.Id, err)))
 			return
 		}
 		total, err := prometheus.NewConstMetric(
@@ -204,7 +208,7 @@ func (e *exporter) Collect(metrics chan<- prometheus.Metric) {
 			prometheus.GaugeValue,
 			float64(lData.MessagesTotal))
 		if err != nil {
-			panic(fmt.Errorf("generating gmail_messages_total total: %s: %v", label.id, err))
+			panic(fmt.Errorf("generating gmail_messages_total total: %s: %v", label.Id, err))
 		}
 		ms <- total
 		unread, err := prometheus.NewConstMetric(
@@ -216,7 +220,7 @@ func (e *exporter) Collect(metrics chan<- prometheus.Metric) {
 			prometheus.GaugeValue,
 			float64(lData.MessagesUnread))
 		if err != nil {
-			panic(fmt.Errorf("generating gmail_messages_unread total: %s: %v", label.id, err))
+			panic(fmt.Errorf("generating gmail_messages_unread total: %s: %v", label.Id, err))
 		}
 		ms <- unread
 	}
@@ -235,7 +239,11 @@ func (e *exporter) startRefreshingLabels() {
 			log.Message("Error doing initial periodic label refresh"),
 			log.Error(err))
 	}
-	fmt.Println("Labels: ", e.refreshingLabels)
+	_ = e.logger.Log(log.Message("Labels initialised"), log.KV{
+		K: "labels",
+		V: e.refreshingLabels,
+	})
+
 	// TODO(gynternet): handle stopping ticker
 	t := time.NewTicker(e.labelRefreshPeriod)
 
@@ -245,7 +253,6 @@ func (e *exporter) startRefreshingLabels() {
 				log.Message("Error doing periodic label refresh"),
 				log.Error(err))
 		}
-		// log here
 		_ = e.logger.Log(log.Message("Labels refreshed"), log.KV{
 			K: "labels",
 			V: e.refreshingLabels,
@@ -256,7 +263,7 @@ func (e *exporter) startRefreshingLabels() {
 const userID = "me"
 
 func (e *exporter) refreshLabels() error {
-	err := e.refresh(e.List(userID).Do)
+	err := e.refresh(e.logger, e.List(userID).Do)
 	if err != nil {
 		refreshLabelsErrs.Inc()
 	}
@@ -266,18 +273,20 @@ func (e *exporter) refreshLabels() error {
 type refreshingLabels []gmailLabel
 
 type gmailLabel struct {
-	name, id, ttype string
+	Name string `json:"name"`
+	Id   string `json:"id"`
+	Type string `json:"type"`
 }
 
 func (l gmailLabel) promLabels() prometheus.Labels {
 	return prometheus.Labels{
-		"label_name": l.name,
-		"label_id":   l.id,
-		"label_type": l.ttype,
+		"label_name": l.Name,
+		"label_id":   l.Id,
+		"label_type": l.Type,
 	}
 }
 
-func (rls *refreshingLabels) refresh(get labelsGetter) error {
+func (rls *refreshingLabels) refresh(logger log.Logger, get labelsGetter) error {
 	// only id, name, messageListVisibility, labelListVisibility, and type are returned by a labels list call
 	// https://developers.google.com/gmail/api/v1/reference/users/labels/list
 	r, err := get()
@@ -287,12 +296,14 @@ func (rls *refreshingLabels) refresh(get labelsGetter) error {
 	if len(r.Labels) == 0 {
 		*rls = nil
 		// TODO: WARN log
-		fmt.Println("No labels found.")
+		if err := logger.Log(log.Message("No labels found.")); err != nil {
+			return errors.Wrap(err, "logging")
+		}
 		return nil
 	}
 	ls := make([]gmailLabel, len(r.Labels))
 	for i, l := range r.Labels {
-		ls[i] = gmailLabel{name: l.Name, id: l.Id, ttype: l.Type}
+		ls[i] = gmailLabel{Name: l.Name, Id: l.Id, Type: l.Type}
 	}
 	*rls = ls
 	return nil
